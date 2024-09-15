364a88ad08d2b166573f511a8c626cbe4c3b8d52
guti10501 <afgutierrez4@gmail.com>
2024-09-15 16:02:25 +0900

    3.8*: Phonebook backend step 8

--- index.js
+++ index.js
@@ -2,7 +2,20 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 app.use(express.json())
-app.use(morgan('tiny'))
+
+app.use(morgan(function (tokens, req, res) {
+  var ret = [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms'
+    ]
+  if (tokens.method(req,res) === "POST") {
+      ret = ret.concat(JSON.stringify(req.body))
+  }
+  return ret.join(' ')
+}))
 
 let persons = [
     { 
